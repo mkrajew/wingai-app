8a7e8f1dcad4246b1ec03cad0cbeff9434dd5b3b
mkrajew <mkrajew3@gmail.com>
2026-02-08 14:20:24 +0100

    added error handling

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ from wings.modeling.loss import DiceLoss
 from wings.config import MODELS_DIR
 
 from wings.utils import load_image
-from wings.visualizing.image_preprocess import (
+from wings.image_preprocess import (
     unet_fit_rectangle_preprocess,
     final_coords,
 )
-from wings.gpa import handle_coordinates
+from wings.gpa import (
+    handle_coordinates,
+    procrustes_align,
+    normalize_shape,
+    center_shape,
+)
 
 
 models = {}
@@ -73,8 +78,10 @@ def root():
 
 
 def process_image(image, x_size, y_size):
-    image_tensor, _, _ = load_image(image, unet_fit_rectangle_preprocess)
-    image_tensor = image_tensor
+    try:
+        image_tensor, _, _ = load_image(image, unet_fit_rectangle_preprocess)
+    except Exception as e:
+        raise LoadImageError("Failed to load image") from e
 
     output = models["model"](image_tensor.to(models["device"]).unsqueeze(0))
     mask = torch.round(output).squeeze().detach().cpu().numpy()
@@ -83,7 +90,31 @@ def process_image(image, x_size, y_size):
     mask_coords = torch.tensor(mask_coords)
     check_carefully = len(mask_coords) < 19 or len(mask_coords) > 22
 
-    coordinates = handle_coordinates(mask_coords, models["shape"])
+    try:
+        coordinates = handle_coordinates(mask_coords, models["shape"])
+        # coordinates[:, 1] = y_size - coordinates[:, 1] - 1
+        # coordinates = coordinates.detach().flatten().long().tolist()
+    except Exception:
+        check_carefully = True
+        if len(mask_coords) > 19:
+            mask_coords = mask_coords[:19]
+        elif len(mask_coords) < 19:
+            missing_points = 19 - len(mask_coords)
+            xmin, ymin = 0, 0
+            xmax, ymax = x_size, y_size
+            random_x = torch.empty(missing_points).uniform_(xmin, xmax)
+            random_y = torch.empty(missing_points).uniform_(ymin, ymax)
+            random_points = torch.stack([random_x, random_y], dim=1)
+            mask_coords = torch.cat([mask_coords, random_points], dim=0)
+        coordinates = mask_coords
+
+    if not check_carefully:
+        gpa = procrustes_align(
+            normalize_shape(center_shape(coordinates)), models["shape"]
+        )
+        gpa_vals = torch.linalg.norm(models["shape"] - gpa, dim=1)
+        check_carefully = gpa_vals.max().item() > 0.04
+
     coordinates[:, 1] = y_size - coordinates[:, 1] - 1
     coordinates = coordinates.detach().flatten().long().tolist()
 
@@ -99,3 +130,9 @@ async def analyze(
 
     coords, check = process_image(encoded, x_size, y_size)
     return JSONResponse(content={"coords": coords, "check": check})
+
+
+class LoadImageError(Exception):
+    """Raised when loading an image fails."""
+
+    pass
